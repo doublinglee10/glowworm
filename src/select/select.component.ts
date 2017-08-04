822045a8aa69fc88192ff5e9e49d58f3dc1360c2
lishichao <lishichao@ourpalm.com>
2017-08-04 15:11:52 +0800

    add select component

---
+++ src/select/select.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'gw-select',
+    templateUrl: './select.component.html'
+})
+export class GWSelectComponent {
+
+}
---
+++ src/select/select.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from "@angular/core";
+import {GWSelectComponent} from "./select.component";
+@NgModule({
+    declarations: [
+        GWSelectComponent
+    ],
+    exports: [
+        GWSelectComponent
+    ]
+})
+export class GWSelectModule {
+}
